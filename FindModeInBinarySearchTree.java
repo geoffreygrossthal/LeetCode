8a03be94bb9287885304a1fdceb8a4ce02ec61a1
geoffreygrossthal <geoffreygrossthal@gmail.com>
2024-05-02 13:41:58 -0700

    Update FindModeInBinarySearchTree.java

--- FindModeInBinarySearchTree.java
+++ FindModeInBinarySearchTree.java
@@ -31,14 +31,55 @@ import java.util.List;
 import java.util.Map;
 
 public class FindModeInBinarySearchTree {
-    
+
     //Method returns the mode(s) of a binary search tree
     public int[] findMode(TreeNode root) {
+
+        //Traverse the tree and add the mdoes to an array list
+        List<Integer> modes_list = new ArrayList<>();
+        //[0] is current value, [1] is max count, [2] is current count
+        int[] values = new int[3];
+        values[0] = 0;
+        values[0] = 0;
+        values[0] = 0;
+        findMode(root, values, modes_list);
+
+        //Add values to array to return
+        int[] modes = new int[modes_list.size()];
+        for (int i = 0; i < modes.length; i++)
+            modes[i] = modes_list.get(i);
+        return modes;
+    }
+
+    //Method is a helper function does inorder traversal (smallest to largest numbers)
+    //and stores the modes (max freqeuncies), inorder traversal is needed becuase it will
+    //consecutively encounters the number from smallest to largest so the freqeuncy of each
+    //element can be counted easisly
+    //Largest to smallest traversal can be applied also
+    private void findMode(TreeNode node, int[] values, List<Integer> modes) {
+        if (node == null)
+            return;
+        findMode(node.left, values, modes);
+        values[2] = (node.val == values[0]) ? values[2] + 1 : 1;
+        if (values[2] == values[1])
+            modes.add(node.val);
+        else if (values[2] > values[1]) {
+            values[1] = values[2];
+            modes.clear();
+            modes.add(node.val);
+        }
+        values[0] = node.val;
+        findMode(node.right, values, modes);
+    }
+
+    
+    //Method returns the mode(s) of a binary search tree
+    public int[] findModeSlower(TreeNode root) {
 
         //HashMap to store numbers with frequencies
         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
         //Recurse through tree to find frequencies
-        findMode(root, frequencyMap);
+        findModeSlower(root, frequencyMap);
 
         //Find max frequency (mode), with number of occurences with the max freqeuncy
         int maxFrequency = 0;
@@ -67,7 +108,7 @@ public class FindModeInBinarySearchTree {
     }
 
     //Method is helper function for counting the frequecnies in a binary tree
-    private void findMode(TreeNode node, HashMap<Integer, Integer> frequencyMap) {
+    private void findModeSlower(TreeNode node, HashMap<Integer, Integer> frequencyMap) {
         //Reach end of path return
         if (node == null)
             return;
@@ -79,8 +120,8 @@ public class FindModeInBinarySearchTree {
             else
                 frequencyMap.put(node.val, 1);
             //Serch left and right branches
-            findMode(node.left, frequencyMap);
-            findMode(node.right, frequencyMap);
+            findModeSlower(node.left, frequencyMap);
+            findModeSlower(node.right, frequencyMap);
         }
     }
 }
