3b607b8420cd9b6293cd3b778e4323301378d721
Geoffrey Grossthal <104024651+geoffreygrossthal@users.noreply.github.com>
2024-06-01 11:45:44 -0700

    Add files via upload

---
+++ ConvertBinaryNumberInLinkedListToInt.java
@@ -0,0 +1,50 @@
+//Date: 6/1/2024
+//Programmer: Geoffrey Grossthal
+//Program: Leetcode problem Convert Binary Number In Linked List to Integer
+
+/*
+Given head which is a reference node to a singly-linked list. 
+The value of each node in the linked list is either 0 or 1. 
+The linked list holds the binary representation of a number.
+Return the decimal value of the number in the linked list.
+The most significant bit is at the head of the linked list.
+
+Example 1:
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+
+Example 2:
+Input: head = [0]
+Output: 0
+ 
+Constraints:
+The Linked List is not empty.
+Number of nodes will not exceed 30.
+Each node's value is either 0 or 1
+*/
+
+public class ConvertBinaryNumberInLinkedListToInt {
+
+    //Method returns the decimal of the binary numbe stored in the list
+    public int getDecimalValue(ListNode head) {
+        int digit, sum = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        storeBinaryOnQueue(queue, head);
+        int position = 0;
+        while (!queue.isEmpty()) {
+            digit = queue.poll();
+            sum += digit * Math.pow(2, queue.size());
+            position++;
+        }
+        return sum;
+    }
+
+    //Method stores the binary number on the queue
+    public void storeBinaryOnQueue(Queue<Integer> queue, ListNode current) {
+        if (current == null)
+            return;
+        queue.offer(current.val);
+        storeBinaryOnQueue(queue, current.next);
+    }
+}
