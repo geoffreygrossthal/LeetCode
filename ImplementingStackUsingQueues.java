92afb1124d46a91d518e8a0481270114eaff2f82
Geoffrey Grossthal <104024651+geoffreygrossthal@users.noreply.github.com>
2024-06-29 06:33:31 -0700

    Add files via upload

---
+++ ImplementingStackUsingQueues.java
@@ -0,0 +1,82 @@
+//Date: 6/29/2024
+//Programmer: Geoffrey Grossthal
+//Program: Leetcode problem Implenting Stack using Queues
+
+/*
+Implement a last-in-first-out (LIFO) stack using only two queues. 
+The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+Implement the MyStack class:
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only push to back, 
+peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate 
+a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ 
+Example 1:
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+ 
+Constraints:
+1 <= x <= 9
+At most 100 calls will be made to push, pop, top, and empty.
+All the calls to pop and top are valid.
+ 
+Follow-up: Can you implement the stack using only one queue?
+*/
+
+
+public class ImplementingStackUsingQueues {
+
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+    
+    //Intialiaze queues with constructor
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    //Pushes element to the top of the stack, will then rerange so it is the last element in q1
+    public void push(int x) {
+        //Add new elements tot q2
+        q2.offer(x);
+        //Move all elements from q1 to q2
+        while (!q1.isEmpty())
+            q2.offer(q1.poll());
+        //Swap q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+    
+    //Removes the last element in the queue which is the top of the stack
+    public int pop() {
+        return q1.poll();
+    }
+    
+    //Returns the last element in the queue which is the top of the stack
+    public int top() {
+        return q1.peek();
+    }
+    
+    //Returns empty if the stack is empty
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
